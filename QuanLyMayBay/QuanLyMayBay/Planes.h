21fe4e41d9b357ed87645823a988738c5300a825
ahuynh0312 <ahuynh359@gmail.com>
2022-03-23 16:13:38 +0700

    Update

---
+++ QuanLyMayBay/QuanLyMayBay/Planes.h
@@ -0,0 +1,37 @@
+#pragma once;
+#define MAX_ID 15
+#define MAX_PLANE 300
+#define MAX_TYPE 40
+
+struct Plane {
+	char idPlane[MAX_ID];
+	char type[MAX_TYPE];
+	int seats;
+};
+class PlaneList {
+private:
+	Plane* data[MAX_PLANE];
+
+public:
+	PlaneList() {
+
+	}
+
+	~PlaneList() {
+		for (int i = 0; i < MAX_PLANE; i++) {
+			delete data[i];
+		}
+	}
+
+	void addPlane() {
+
+	}
+
+	void deletePlane() {
+
+	}
+
+	void editPlane() {
+
+	}
+};
--- QuanLyMayBay/QuanLyMayBay/main.cpp
+++ QuanLyMayBay/QuanLyMayBay/main.cpp
@@ -1,26 +1,22 @@
 
 #include"UIController.h"
-using namespace std;
 int main()
 {
 	init();
-	//DT comment
-	//DT COMMETN TIEP
-	
+
 	while (true) {
 		setactivepage(1 - getactivepage());
-		createWindow();
-		//eventHandle(t);
-	
+		onUpdate();
 
-		clearmouseclick(WM_LBUTTONDOWN);
+
+		//clearmouseclick(WM_LBUTTONDOWN);
 		setvisualpage(getactivepage());
 		delay(20);
 
 	}
 	getch();
 	closegraph();
-	
+
 	return 0;
 }
 
