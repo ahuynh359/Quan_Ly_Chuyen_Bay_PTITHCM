77738e5cd98ba6e80d5881fe71dbbd020b6fa043
pthuydathao <pthuydathao@gmail.com>
2022-03-25 14:31:01 +0700

    update

--- QuanLyMayBay/QuanLyMayBay/Planes.h
+++ QuanLyMayBay/QuanLyMayBay/Planes.h
@@ -2,16 +2,16 @@
 #define MAX_ID 15
 #define MAX_PLANE 300
 #define MAX_TYPE 40
+#include<iostream>
+using namespace std;
 
 struct Plane {
 	char idPlane[MAX_ID];
 	char type[MAX_TYPE];
 	int seats;
 };
-class PlaneList {
-private:
-	Plane* data[MAX_PLANE];
 
+<<<<<<< Updated upstream
 public:
 	PlaneList() {
 		for (int i = 0; i < MAX_PLANE; i++) {
@@ -30,10 +30,25 @@ public:
 	}
 
 	void deletePlane() {
+=======
+struct PlaneList {
+	int size = 0;
+	Plane* data[MAX_PLANE]; 
 
-	}
-
-	void editPlane() {
-
-	}
 };
+>>>>>>> Stashed changes
+
+void add(PlaneList &list) {
+	Plane* plane = new Plane();
+	cout << "Nhap so hieu MB:";
+	cin >> plane->idPlane;
+	cout << "Nhap loai may bay:";
+	cin >> plane->type;
+	while (plane->seats < 20) {
+		cout << "Nhap so cho:";
+		cin >> plane->seats;
+	}
+	list.data[(++list.size)] = plane;
+}
+
+
