00fec126c2ec3228941143d2b01d8ff83acf8e5e
mynameisnhi <tuyetnhi260802@gmail.com>
2022-04-09 16:22:47 +0700

    Update UIController.h

--- QuanLyMayBay/QuanLyMayBay/UIController.h
+++ QuanLyMayBay/QuanLyMayBay/UIController.h
@@ -22,7 +22,7 @@ public:
 		int x = TAB_LEFT;
 		int w = x + TAB_WIDTH;
 		for (int i = 0; i < MAX_TAB; i++) {
-			
+
 			t[i] = Tab(x, TAB_TOP, w, TAB_HEIGHT, TAB_TEXT[i], TAB_TEXT_COLOR);
 
 			x = w + TAB_SPACE;
@@ -81,69 +81,4 @@ public:
 
 
 
-
-#include "BookTicketTab.h"
-#include "Button.h"
-#include "ManagePlanesTab.h"
-#include "Tab.h"
-
-class UIController {
-private:
-  Tab t[MAX_TAB];
-  ManagePlanesTab managePlaneTab;
-  BookTicket bookTicketTab;
-  Tab *temp;
-
-public:
-  UIController() {
-    initwindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Manage Flights");
-    // Khoi tao tab
-    int x = TAB_LEFT;
-    int w = x + TAB_WIDTH;
-    for (int i = 0; i < MAX_TAB; i++) {
-
-      t[i] = Tab(x, TAB_TOP, w, TAB_HEIGHT, TAB_TEXT[i], TAB_TEXT_COLOR);
-
-      x = w + TAB_SPACE;
-      w += TAB_WIDTH + TAB_SPACE;
-    }
-    // managePlaneTab = ManagePlanesTab();
-
-    temp = &t[0];
-  }
-  ~UIController() { delete temp; }
-
-  void drawSubMenu() {
-    setbkcolor(TAB_ON_SELECTED_BACKGROUND);
-    setfillstyle(SOLID_FILL, SUBWINDOW_BACKGROUND);
-    bar(SUBWINDOW_LEFT, SUBWINDOW_TOP, SUBWINDOW_RIGHT, SUBWINDOW_BOTTOM);
-  }
-
-  void drawBackground() {
-    setbkcolor(BACKGROUND_COLOR);
-    cleardevice();
-    setfillstyle(SOLID_FILL, BACKGROUND_COLOR);
-    bar(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-  }
-
-  void onUpdate() {
-    drawBackground();
-    drawSubMenu();
-    for (int i = 0; i < MAX_TAB; i++) {
-      t[i].onAction(temp);
-    }
-
-    if (temp == &t[0]) {
-      managePlaneTab.drawUI();
-
-    } else if (temp == &t[1]) {
-      managePlaneTab.reset();
-
-    } else if (temp == &t[2]) {
-
-      managePlaneTab.reset();
-      bookTicketTab.drawUI();
-    }
-  }
- main
 };
